ce1ba284d5fe262c9cb2004afaffaa9d579ed01a
RaulGal <ragaher20@gmail.com>
2021-09-02 09:50:20 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonInfoComponent } from './pokemon-info/pokemon-info.component';
 import { InfoPokedexService } from './pokedexservice.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   declarations: [
     AppComponent,
     HelloComponent,
